e19bf26eeaf180127ae62e720dbe3b7211e372ee
Lukas Kristensen <lukashtxb16@gmail.com>
2022-02-28 01:26:17 +0100

    import dataset

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+
+
+def formatData(datasetPath):
+    with open(datasetPath) as document:
+        dataset = document.read().splitlines()
+        return dataset
 
 
 def main(dataset):
-    if dataset is not None:
-        print(f"Evaluating dataset with the length {len(dataset)}")
+    if dataset is None:
+        print("Did not register a dataset")
+        return
+    print(f"Evaluating dataset with the length {len(dataset)}")
+
 
 if __name__ == '__main__':
-    main()
+    nasdaq71_22 = 'data-set/^IXIC 1971-2022.csv'
+    datasetFormatted = formatData(nasdaq71_22)
+    main(datasetFormatted)
