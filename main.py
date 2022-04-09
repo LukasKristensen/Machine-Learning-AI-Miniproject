06d2506918cfd564a0fbb2884229984ced3c6c59
Lukas Kristensen <lukashtxb16@gmail.com>
2022-04-09 02:44:57 +0200

    some progress

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 import numpy as np
+from matplotlib import pyplot as plt
 
-# Approaches: Deep Learning, Broker Fees, Discrete Fourier Transformation
-
+import pandas as pd
+from sklearn.cluster import KMeans
 
 def formatData(datasetPath):
     with open(datasetPath) as document:
@@ -14,25 +15,44 @@ def formatData(datasetPath):
         return dataset
 
 
-def main(dataset):
+def regression(dataset):
     if dataset is None:
         print("Did not register a dataset")
         return
     print(f"Evaluating dataset with the length {len(dataset)}")
     print(dataset[0])
     print(dataset[1])
-    print(dataset[len(dataset)-1])
+    print(dataset[-1])
 
-def kmeansclustering(dataset):
-    print("Clustering Dataset:",dataset)
+def kmeansclustering(datasetPath):
+    print("Clustering Dataset:",datasetPath)
+
+    pureStock = {'TICK':['MSFT','AAPL','BABA','NVDA'],
+                 '5YAVG':[85.71,89.02,0.03,128.06],
+                 'BETA':[0.91,1.19,0.89,1.42]}
+
+    stockData = pd.read_csv(datasetPath,index_col=0)
+    stockDataFrame = pd.DataFrame(pureStock)
+    print(stockDataFrame.head())
+
+
+    frameInertia = []
+    for k in range(2, 4):
+        kMeans = KMeans(n_clusters=k)
+        kMeans.fit(stockDataFrame)
+        frameInertia.append(kMeans.inertia_)
+
+    figureInertia = plt.figure(figusize=(10,5))
+    plt.plot(range(2,4), frameInertia)
+    plt.grid(True)
+    plt.show()
+
 
 
 
 if __name__ == '__main__':
     nasdaq71_22 = 'data-set/^IXIC 1971-2022.csv'
     datasetFormatted = formatData(nasdaq71_22)
-    main(datasetFormatted)
+    regression(datasetFormatted)
 
-
-    clusterDataset = formatData('data-set/stockMarketData.csv')
-    kmeansclustering(clusterDataset)
+    kmeansclustering('data-set/testingData.csv')
