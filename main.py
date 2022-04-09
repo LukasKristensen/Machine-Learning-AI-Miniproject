a59767f8663f17d478c25c9ce8251413d72c1175
Lukas Kristensen <lukashtxb16@gmail.com>
2022-04-10 00:08:26 +0200

    Added PE values

--- main.py
+++ main.py
@@ -26,20 +26,24 @@ def regression(dataset):
     print(dataset[-1])
 
 def kmeansclustering(datasetPath):
-    """"
+    """
     The clustering structure is based on the code from:
     https://pythonforfinance.net/2018/02/08/stock-clusters-using-k-means-algorithm-in-python/
     """
 
     ticker = ['MSFT', 'AAPL', 'BABA', 'NVDA', 'PINS', 'ZM', 'SHOP', 'PYPL', 'MA', 'ADBE', 'TSLA']
-    Y5AVG = [85.71,89.02,0.03,128.06,0.01,0.08,31.32,43.6,57.33,62.79,300.59]
-    BETA = [0.91,1.19,0.89,1.42,1.19,-0.94,1.62,1.29,1.08,1.05,2.08]
+    Y5AVG = [85.71,89.02,0.03,128.06,0.01,0.08,31.32,43.6,57.33,62.79]
+    BETA = [0.91,1.19,0.89,1.42,1.19,-0.94,1.62,1.29,1.08,1.05]
+    PE = [31.61,28.28,27.54,60.05,51.2,24.97,26.33,31.59,40.21,44.23]
+
+    print(len(Y5AVG),len(BETA),len(PE))
 
     stockDataFrame = pd.DataFrame(Y5AVG)
     stockDataFrame.columns = ['Y5AVG']
     stockDataFrame['BETA'] = BETA
+    stockDataFrame['PE'] = PE
 
-    data = np.asarray([np.asarray(stockDataFrame['Y5AVG']), np.asarray(stockDataFrame['BETA'])]).T
+    data = np.asarray([np.asarray(stockDataFrame['Y5AVG']), np.asarray(stockDataFrame['BETA']), np.asarray(stockDataFrame['PE'])]).T
     dataCopy = data
 
     print(stockDataFrame.head())
@@ -64,7 +68,6 @@ def kmeansclustering(datasetPath):
                    data[assignedStock==3,0], data[assignedStock==3,1], 'or')
 
     kmeanPlot.plot(stockCentroids[:,0], stockCentroids[:,1],'sg',markersize=10, alpha=0.1)
-    kmeanPlot.ylabel = ('testing')
 
     for i in range(len(assignedStock)):
         print(assignedStock[i], ticker[i])
