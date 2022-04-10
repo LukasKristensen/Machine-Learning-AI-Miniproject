c450d8c3060e099899198844318bb6e735111801
Lukas Kristensen <lukashtxb16@gmail.com>
2022-04-10 15:43:26 +0200

    Better code

--- main.py
+++ main.py
@@ -10,11 +10,6 @@ from scipy.cluster.vq import kmeans,vq
 import pandas as pd
 from sklearn.cluster import KMeans
 
-def formatData(datasetPath):
-    with open(datasetPath) as document:
-        dataset = document.read().splitlines()
-        return dataset
-
 
 def regression(dataset):
     if dataset is None:
@@ -35,58 +30,55 @@ def kmeansclustering(stocksCSV):
     PE = stocksCSV['PE']
 
     stockDataFrame = pd.DataFrame(Y5AVG)
-    stockDataFrame.columns = ['Y5AVG']
+    stockDataFrame['Y5AVG'] = Y5AVG
     stockDataFrame['BETA'] = BETA
     stockDataFrame['PE'] = PE
+    stockDataFrame['TICKER'] = ticker
 
     data = np.asarray([np.asarray(stockDataFrame['Y5AVG']), np.asarray(stockDataFrame['BETA']),np.asarray(stockDataFrame['PE'])]).T
     dataCopy = data
 
-    print(stocksCSV.head())
+    numberOfClusters = 3
 
     frameInertia = []
-    for k in range(2, 6):
+    for k in range(1, numberOfClusters+1):
         kMeans = KMeans(n_clusters=k)
         kMeans.fit(dataCopy)
         frameInertia.append(kMeans.inertia_)
 
-    plt.plot(range(2,6), frameInertia)
+    plt.plot(range(1,numberOfClusters+1), frameInertia)
     plt.grid(True)
     plt.show()
 
-    stockCentroids,_ = kmeans(data, 3)
+    stockCentroids,_ = kmeans(data, numberOfClusters)
     assignedStock,_ = vq(data, stockCentroids)
 
-    fig3d = plt.figure(figsize=(15,15))
+    for i in range(len(assignedStock)):
+        print(assignedStock[i], ticker[i])
+
+    print("View:", "\n", stockDataFrame, assignedStock)
+
+    stockDataFrame['CLUSTER'] = assignedStock
+    print('Clustered:\n',stockDataFrame)
+
+    fig3d = plt.figure(figsize=(9,7))
     axes = fig3d.add_subplot(111, projection='3d')
     axes.scatter(data[assignedStock==0,0], data[assignedStock==0,1],data[assignedStock==0,2], s=20, c='blue',label="Cluster 0")
     axes.scatter(data[assignedStock==1,0], data[assignedStock==1,1],data[assignedStock==1,2], s=20, c='green',label="Cluster 1")
     axes.scatter(data[assignedStock==2,0], data[assignedStock==2,1],data[assignedStock==2,2], s=20, c='red',label="Cluster 2")
     axes.scatter(data[assignedStock==3,0], data[assignedStock==3,1],data[assignedStock==3,2], s=20, c='yellow',label="Cluster 3")
-    axes.set_xlabel('5YAvgReturn')
+    axes.scatter(stockCentroids[:,0], stockCentroids[:,1], stockCentroids[:,2], s=80, c='black', alpha=0.3,label= "Centroids")
+    axes.set_xlabel('AvgAnnualReturn(5Y)')
     axes.set_ylabel('Beta')
     axes.set_zlabel('PE')
+
     plt.show()
 
-    for i in range(len(assignedStock)):
-        print(assignedStock[i], ticker[i])
-
-    """kmeanPlot.show()
-    plt.show()"""
-    print("View:","\n", stockDataFrame,assignedStock)
-
-
 if __name__ == '__main__':
-    nasdaq71_22 = 'data-set/^IXIC 1971-2022.csv'
-    datasetFormatted = formatData(nasdaq71_22)
-
     filepathNasdaq = (open('data-set/^IXIC 1971-2022.csv'))
     nasdaqCSV = pd.read_csv(filepathNasdaq)
+    regression(nasdaqCSV)
 
     filepathStocks = (open('data-set/testDataV3.csv'))
     stocksCSV = pd.read_csv(filepathStocks, sep=';')
-
-    regression(nasdaqCSV)
-
-    # kmeansclustering('data-set/testingData.csv')
     kmeansclustering(stocksCSV)
