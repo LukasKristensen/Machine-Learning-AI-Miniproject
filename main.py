20a1317a4fab6939dbef5dcf23599fe88f962001
Lukas Kristensen <lukashtxb16@gmail.com>
2022-04-21 00:04:07 +0200

    cleanup

--- main.py
+++ main.py
@@ -12,8 +12,7 @@ from sklearn.metrics import explained_variance_score
 
 # Todo: Comment code before hand-in
 
-
-def kmeansclustering(stocksCSV):
+def kMeansClustering(stocksCSV):
     """
     The clustering code structure is based on the code from:
     https://pythonforfinance.net/2018/02/08/stock-clusters-using-k-means-algorithm-in-python/
@@ -73,12 +72,7 @@ def kmeansclustering(stocksCSV):
 
 
 def regression(dataset):
-    """
-    Interesting ways to implementing regression:
-    - Support Vector Machine (Should be the first on to try out)
-    - Convolutional Neural Network
-    - Recurrent Neural Network
-
+    """"
     Material for implementing:
     - https://www.analyticsvidhya.com/blog/2020/03/support-vector-regression-tutorial-for-machine-learning/
     - https://medium.com/pursuitnotes/support-vector-regression-in-6-steps-with-python-c4569acd062d
@@ -91,7 +85,7 @@ def regression(dataset):
         return
 
     daysPredict = 10
-
+    datasetInterval = -3000
 
     nasdaqDF = pd.DataFrame()
     nasdaqDF['Open'] = dataset['Open'].astype(int)
@@ -99,35 +93,35 @@ def regression(dataset):
     nasdaqDF['Prediction'] = dataset['Open'].shift(-daysPredict)
     nasdaqDF = nasdaqDF[:-daysPredict]
 
-    plt.plot(dataset['Date'][-3000:], dataset['Open'][-3000:])
-    plt.xticks(range(0,len(dataset['Date'][-3000:]),300))
+    plt.plot(dataset['Date'][datasetInterval:], dataset['Open'][datasetInterval:])
+    plt.xticks(range(0,len(dataset['Date'][datasetInterval:]),300))
     plt.title("Nasdaq Composite 1971-2022")
     plt.show()
 
-    trainingData = nasdaqDF[-3000:-1000]
+    trainingData = nasdaqDF[datasetInterval:-1000]
     testData = nasdaqDF[-1000:]
 
-    xreshaped = np.array(trainingData['Open']).reshape(-1, 1)
-    yreshaped = np.array(trainingData['Prediction'])
+    xReshaped = np.array(trainingData['Open']).reshape(-1, 1)
+    yReshaped = np.array(trainingData['Prediction'])
 
-    xtestData = np.array(testData['Open']).reshape(-1, 1)
-    ytestData = np.array(testData['Prediction'])
+    xTestData = np.array(testData['Open']).reshape(-1, 1)
+    yTestData = np.array(testData['Prediction'])
 
     """
-    Next 5 lines (Regression model) is based on the documentation from: 
+    Next 6 lines (SVR-Model) is based on the documentation from: 
     https://scikit-learn.org/stable/modules/generated/sklearn.svm.SVR.html"""
-    modelRegression = make_pipeline(StandardScaler(), SVR(kernel='rbf',C=2000, gamma=0.02))
-    fittedRegression = modelRegression.fit(xreshaped, yreshaped)
+    modelRegression = make_pipeline(StandardScaler(), SVR(kernel='rbf', C=2000, gamma=0.01))
+    fittedRegression = modelRegression.fit(xReshaped, yReshaped)
 
-    predictionRegression = fittedRegression.predict(xtestData)
-    scoreReg = explained_variance_score(ytestData, predictionRegression)
+    predictionRegression = fittedRegression.predict(xTestData)
+    scoreReg = explained_variance_score(yTestData, predictionRegression)
 
     plt.plot(trainingData['Date'],trainingData['Prediction'], c="blue")
     plt.plot(testData['Date'],predictionRegression, c="red")
-    plt.plot(testData['Date'],ytestData, c="green")
-    plt.xticks(range(0, len(nasdaqDF['Date'][-3000:]), 300))
+    plt.plot(testData['Date'],yTestData, c="green")
+    plt.xticks(range(0, len(nasdaqDF['Date'][datasetInterval:]), 300))
 
-    print("Done test regression", scoreReg)
+    print("Variance score for regression:", scoreReg)
     plt.show()
 
 
@@ -138,4 +132,4 @@ if __name__ == '__main__':
 
     filepathStocks = (open('data-set/testDataV3.csv'))
     stocksToCSV = pd.read_csv(filepathStocks, sep=';')
-    kmeansclustering(stocksToCSV)
+    kMeansClustering(stocksToCSV)
